61813fd61f682f3efa4a5862b87a9404d0ff1214
PHODER1LAPTOP\AlonT SuperSky <alontalmi@supersky.games>
2022-06-21 11:21:28 +0300

    Turns and map

---
+++ Runtime/Turns/Turns.cs
@@ -0,0 +1,50 @@
+using Phoder1.Core.Attributes;
+using Phoder1.Patterns;
+using System;
+using System.Collections;
+using UniRx;
+using UnityEngine;
+
+namespace Phoder1.SpaceEmpires
+{
+    public interface ITurns
+    {
+        IReactiveProperty<float> TurnsPerSecond { get; }
+        IReadOnlyReactiveProperty<int> TurnCount { get; }
+        TimeSpan TurnLength { get; }
+        TokenMachine Active { get; }
+        void StartTurns();
+        IDisposable Pause() => Active.GetToken();
+    }
+    [Serializable]
+    public class Turns : ITurns
+    {
+        [SerializeField, Inline(true)]
+        private ReactiveProperty<float> turnsPerSecond = new ReactiveProperty<float>(1);
+
+        private readonly ReactiveProperty<int> turnCount = new ReactiveProperty<int>(0);
+
+        public IReadOnlyReactiveProperty<int> TurnCount => turnCount;
+
+        public IReactiveProperty<float> TurnsPerSecond => turnsPerSecond;
+
+        public TimeSpan TurnLength { get; private set; }
+
+        public TokenMachine Active => new TokenMachine(null, null);
+
+        public void StartTurns()
+        {
+            MainThreadDispatcher.StartCoroutine(CallTurn());
+        }
+
+        private IEnumerator CallTurn()
+        {
+            TurnLength = TimeSpan.FromSeconds(1 / TurnsPerSecond.Value);
+            turnCount.Value++;
+
+            yield return new WaitForSeconds((float)TurnLength.TotalSeconds);
+
+            yield return CallTurn();
+        }
+    }
+}
